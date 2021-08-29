0b3f1e96d6108fc4c7a57c8942276d8440679973
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-08-29 19:58:36 +0900

    2021-08-29 오영준

---
+++ src/main/java/com/jasper/learningspringfinal/SpringConfig.java
@@ -0,0 +1,32 @@
+package com.jasper.learningspringfinal;
+
+import com.jasper.learningspringfinal.repository.JdbcMemberRepository;
+import com.jasper.learningspringfinal.repository.MemberRepository;
+import com.jasper.learningspringfinal.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
+
+
+}
--- src/main/java/com/jasper/learningspringfinal/controller/MemberController.java
+++ src/main/java/com/jasper/learningspringfinal/controller/MemberController.java
@@ -41,4 +41,6 @@ public class MemberController {
         return "members/memberList";
     }
 
+
+
 }
